ca2d5c45bf7bd4142eca7b6a8ca6035a2af38b0c
gojeta786 <gokuvegetadbz111@gmail.com>
2026-05-11 21:40:30 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <main style={{ padding: "20px" }}>
-      <h1>Her Budget 💸</h1>
-      <p>Your personal budget planner</p>
-    </main>
-  );
+  return <h1>Hello</h1>;
 }
